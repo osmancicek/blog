0a6b3977756b387554e3400cdfe1e84a263bd7a6
Osman <osman.cicek93@gmail.com>
2016-10-08 10:33:35 +0300

    list all form results

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,10 +15,17 @@ public
     params.require(:article).permit(:title, :text)
   end
 
-  def show
-     @article = Article.find(params[:id])
-   end
 
-   def new
-   end
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
 end
